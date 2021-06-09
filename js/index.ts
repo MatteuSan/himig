724e5d8d406b0b96acdd3dbd08007cbae76c1cfc
Matteu <matteugt@gmail.com>
2021-06-09 21:18:27 +0800

    Add TS

--- js/index.ts
+++ js/index.ts
@@ -1,43 +1,56 @@
-"use strict";
-var header = document.querySelector('.header--scrollable');
-var banner = document.querySelector('.banner');
-var options = {
+const header: any = document.querySelector('.header--scrollable')
+const banner: any = document.querySelector('.banner')
+
+const options = {
     rootMargin: "-100px 0px 0px 0px",
-};
-var observer = new IntersectionObserver(function (entries, observer) {
-    entries.forEach(function (entry) {
-        if (!entry.isIntersecting) {
+}
+
+const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
             header.classList.add('scrolled');
-        }
-        else {
+        } else {
             header.classList.remove('scrolled');
         }
     });
 }, options);
-observer.observe(banner);
-var search = document.querySelector('.search');
-var searchButton = document.querySelector('.search-button');
-var closeButton = document.querySelector('.close-button');
-var headerText = document.querySelector('.header__text');
+
+observer.observe(banner)
+
+const search: any = document.querySelector('.search')
+const searchButton: any = document.querySelector('.search-button')
+const closeButton: any = document.querySelector('.close-button')
+const headerText: any = document.querySelector('.header__text')
+
 searchButton.addEventListener('click', function () {
-    headerText.classList.add('invisible');
-    searchButton.classList.add('invisible');
-    search.classList.add('visible');
-    closeButton.classList.add('visible');
+
+    headerText.classList.add('invisible')
+    searchButton.classList.add('invisible')
+
+    search.classList.add('visible')
+    closeButton.classList.add('visible')
+
 });
+
 closeButton.addEventListener('click', function () {
-    search.classList.remove('visible');
-    closeButton.classList.remove('visible');
-    headerText.classList.remove('invisible');
-    searchButton.classList.remove('invisible');
+
+    search.classList.remove('visible')
+    closeButton.classList.remove('visible')
+
+    headerText.classList.remove('invisible')
+    searchButton.classList.remove('invisible')
+
 });
+
 // CHIP__OPTIONS
+
 /*const optionsTrigger = document.querySelector('.chip__options__trigger');
 const optionsMenu = document.querySelector('.chip__options');
 
 optionsTrigger.addEventListener('click', function() {
     this.parentElement.firstChild
 });*/
+
 // @ts-ignore
 $(document).ready(function () {
     // @ts-ignore
@@ -45,13 +58,12 @@ $(document).ready(function () {
         // @ts-ignore
         $(this).parent().children('.chip__options').slideToggle("fast");
         // @ts-ignore
-        var option = $(this).next();
+        let option = $(this).next();
         if (option.hasClass("visible")) {
             // @ts-ignore
             $(this).html("menu");
             option.removeClass("visible");
-        }
-        else {
+        } else {
             // @ts-ignore
             $(this).html("close");
             option.addClass("visible");
