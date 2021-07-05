4fe4feb567366d00a33a8c828c6981ba2b83de92
Matteu <matteugt@gmail.com>
2021-07-05 11:28:36 +0800

    Design fixes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Chip_1 = require("../src/js/structs/Chip");
-new Chip_1.default().call();
+var optionsTrigger = '.chip__options__trigger';
+var optionsClass = '.chip__options';
+var openIcon = 'menu';
+var closeIcon = 'close';
+// @ts-ignore
+$(document).ready(function () {
+    // @ts-ignore
+    $(optionsTrigger).on("click", function () {
+        // @ts-ignore
+        $(this).parent().children(optionsClass).slideToggle("fast");
+        // @ts-ignore
+        var option = $(this).next();
+        if (option.hasClass("visible")) {
+            // @ts-ignore
+            $(this).html(openIcon);
+            option.removeClass("visible");
+        }
+        else {
+            // @ts-ignore
+            $(this).html(closeIcon);
+            option.addClass("visible");
+        }
+    });
+});
--- js/index.ts
+++ js/index.ts
@@ -1,2 +1,25 @@
-import Chip from '../src/js/structs/Chip';
-new Chip().call();
+const optionsTrigger = '.chip__options__trigger';
+const optionsClass = '.chip__options';
+const openIcon = 'menu';
+const closeIcon = 'close'
+
+// @ts-ignore
+$(document).ready(function () {
+    // @ts-ignore
+    $(optionsTrigger).on("click", function () {
+        // @ts-ignore
+        $(this).parent().children(optionsClass).slideToggle("fast");
+        // @ts-ignore
+        let option = $(this).next();
+        if (option.hasClass("visible")) {
+            // @ts-ignore
+            $(this).html(openIcon);
+            option.removeClass("visible");
+        }
+        else {
+            // @ts-ignore
+            $(this).html(closeIcon);
+            option.addClass("visible");
+        }
+    });
+});
