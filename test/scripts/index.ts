e93e87ac09b62f7358c9150062d13091205c79d9
Matteu <matteugt@gmail.com>
2021-09-23 19:38:31 +0800

    Initial v3.0.0 commit

--- test/scripts/index.ts
+++ test/scripts/index.ts
@@ -22,8 +22,8 @@
  *
  */
 
-import SCHeader from '../../src/Components/SCHeader/component';
-import SCDrawer from "../../src/Components/SCDrawer/component";
+import SCHeader from '../../src/SCHeader/component';
+import SCDrawer from "../../src/SCDrawer/component";
 
 new SCDrawer().render();
 new SCHeader().render({
