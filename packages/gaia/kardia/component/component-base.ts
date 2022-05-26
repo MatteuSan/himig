6207abcf5716782c005f8e94aa81ccc8c423f11b
Matteu <matteugt@gmail.com>
2022-05-26 22:45:46 +0800

    FEAT: Add custom property querying

--- packages/gaia/kardia/component/component-base.ts
+++ packages/gaia/kardia/component/component-base.ts
@@ -21,15 +21,24 @@
  */
 
 import { GSComponent } from './types/gs-component';
+import { accessProperty } from './component-properties';
 
 export class GSComponentBase {
 
   protected GS_TARGET_COMPONENT: Element | HTMLElement | null;
   protected GS_REFERENCE_COMPONENT: GSComponent;
+  protected GS_CUSTOM_PROPERTIES: string[];
+  private readonly GS_COMPONENT_NAME: string;
 
-  constructor(targetComponent: Element | HTMLElement | null, referenceComponent: GSComponent) {
+  constructor(
+    targetComponent: Element | HTMLElement | null,
+    referenceComponent: GSComponent,
+    customProperties: string[],
+  ) {
     this.GS_TARGET_COMPONENT = targetComponent;
     this.GS_REFERENCE_COMPONENT = referenceComponent;
+    this.GS_CUSTOM_PROPERTIES = customProperties;
+    this.GS_COMPONENT_NAME = this.GS_REFERENCE_COMPONENT.substring(3);
   }
 
   addClass(className: string): void {
@@ -54,6 +63,15 @@ export class GSComponentBase {
 
   getReference(): GSComponent {
     return this.GS_REFERENCE_COMPONENT;
+  }
+
+  getCustomProperty(query: string): string {
+    const prop: string = this.GS_CUSTOM_PROPERTIES[this.GS_CUSTOM_PROPERTIES.indexOf(query)];
+    return accessProperty(`${this.GS_COMPONENT_NAME}-${prop}`);
+  }
+
+  getCustomProperties(): string[] {
+    return this.GS_CUSTOM_PROPERTIES;
   }
 
   getId(): string|undefined {
