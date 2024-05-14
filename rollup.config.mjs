ad5dc3d25eec75e2b17f2c8456e54045b06ae7d1
Matteu <matteugt@gmail.com>
2024-05-15 00:16:43 +0800

    UPDATE: Modify web-components build steps

---
+++ rollup.config.mjs
@@ -0,0 +1,29 @@
+/*
+ * @license
+ * Copyright (c) 2024 MatteuSan
+ * SPDX-License-Identifier: MIT
+ */
+
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import multiEntry from '@rollup/plugin-multi-entry';
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+  input: [
+    'packages/himig-components/ms-button.js'
+  ],
+  output: {
+    dir: 'packages/himig-components',
+    format: 'esm'
+  },
+  plugins: [
+    multiEntry({
+      entryFileName: 'ms-button.js'
+    }),
+    resolve(),
+    babel({ babelHelpers: 'bundled' }),
+    terser()
+  ]
+};
